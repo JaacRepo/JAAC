81a23d3ec750c60cb7acc52dc4a694630db1df81
vlad <serbanescu.vlad.nicolae@gmail.com>
2018-03-13 23:38:24 +0100

    Refactor Priotity names

--- src/main/java/abs/api/cwi/Actor.java
+++ src/main/java/abs/api/cwi/Actor.java
@@ -5,15 +5,15 @@ import java.util.concurrent.Callable;
 public interface Actor {
 	boolean STRICT = true;
 	boolean NON_STRICT = false;
-	int LOW_PRIORITY = 0;
-	int HIGH_PRIORITY = 1;
+	int LOW = 0;
+	int HIGH = 1;
 
 	<V> ABSFuture<V> send(Callable<ABSFuture<V>> message);
 	<V> ABSFuture<V> spawn(Guard guard, Callable<ABSFuture<V>> message);
 	<T,V> ABSFuture<T> getSpawn(ABSFuture<V> f, CallableGet<T, V> message, int priority, boolean strict);
 
 	default <T, V> ABSFuture<T> getSpawn(ABSFuture<V> f, CallableGet<T, V> message) {
-		return getSpawn(f, message, LOW_PRIORITY, NON_STRICT);
+		return getSpawn(f, message, LOW, NON_STRICT);
 	}
 
 	default int compare(Actor o) {
