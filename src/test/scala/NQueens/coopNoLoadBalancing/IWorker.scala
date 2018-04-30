dcc7b2c40819e0273ca3eadf735d6fed57d0681c
Mahdi <mmajid@gmail.com>
2018-04-30 15:48:58 +0200

    deleting other examples for now
    fix the compile errors later with the new syntax

--- src/test/scala/NQueens/coopNoLoadBalancing/IWorker.scala
+++
@@ -1,7 +0,0 @@
-package NQueens.coopNoLoadBalancing
-
-import abs.api.cwi._
-
-trait IWorker extends TypedActor[IWorker] {
-  def nqueensKernelPar( list : Array[Int],  depth : Int,  priority : Int): Message[List[Array[Int]]]
-}
