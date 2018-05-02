b72ce1a132f7cbe5f64283634bd536911965b82c
Mahdi <mmajid@gmail.com>
2018-05-02 13:12:31 +0200

    sieve example with new syntax

---
+++ src/test/scala/eratosthenes/Sieve.scala
@@ -0,0 +1,36 @@
+package eratosthenes
+
+import abs.api.cwi.ABSFuture.done
+import abs.api.cwi.{ActorSystem, TypedActor}
+
+class Sieve(prime: Int) extends TypedActor {
+  var next: Option[Sieve] = None
+
+  def divide(toDivide: Int): Future[Option[Int]] = messageHandler {
+    if (toDivide % prime == 0) {
+      done(None)  // no prime here
+    } else {
+      next match {
+        case None =>
+          next = Some(new Sieve(toDivide))
+          done(Some(toDivide))  // found a prime 
+        case Some(nextPrime) =>
+          nextPrime.divide(toDivide)  // delegate
+      }
+    }
+  }
+}
+
+object SieveMain extends TypedActor {
+  def main(args: Array[String]): Unit = {
+    val two = new Sieve(2)
+    val futures = for (i <- 3 to 1000) yield {two.divide(i)}
+    sequence(futures) onSuccess { results: List[Option[Int]] =>
+      val primes = 2 +: results.flatten
+      println(s"found ${primes.size} primes: " + primes.mkString("[", ", ", "]"))
+      ActorSystem.shutdown()
+      done
+    }
+  }
+}
+
