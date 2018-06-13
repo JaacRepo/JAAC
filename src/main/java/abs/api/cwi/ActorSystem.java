b69262d12aae34568d41e44dc92e38187278a1ac
Mahdi Jaghouri <mmajid@gmail.com>
2018-06-13 16:07:29 +0200

    fix our forkjoin pool instance to use non-daemon threads

--- src/main/java/abs/api/cwi/ActorSystem.java
+++ src/main/java/abs/api/cwi/ActorSystem.java
@@ -2,11 +2,12 @@
 package abs.api.cwi;
 
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinWorkerThread;
 import java.util.concurrent.RecursiveAction;
 
 public class ActorSystem {
 	/** The main executor. */
-	public static ForkJoinPool mainExecutor = new ForkJoinPool(10);
+	private static ForkJoinPool mainExecutor = new ForkJoinPool(10, NonDaemonForkJoinWorkerThread::new, null, false);
 
 	private ActorSystem() { }
 
@@ -17,4 +18,11 @@ public class ActorSystem {
 	public static void shutdown() {
 		mainExecutor.shutdown();
 	}
+
+	private static class NonDaemonForkJoinWorkerThread extends ForkJoinWorkerThread {
+        NonDaemonForkJoinWorkerThread(ForkJoinPool pool) {
+            super(pool);
+            this.setDaemon(false);
+        }
+    }
 }
