de35b77148746e7b790986da26f99d1506827b60
Mahdi Jaghouri <mjaghouri@ebay.com>
2018-09-22 20:44:47 +0200

    remove and/or guard generators

--- src/main/java/com/ascoop/Guard.java
+++ src/main/java/com/ascoop/Guard.java
@@ -13,38 +13,40 @@ public abstract class Guard {
 		return new FutureGuard<>(f, awaitingActors);
 	}
 
-	static private Guard convert(Object o) {
-		if (o instanceof Supplier) {
-			return convert((Supplier<Boolean>) o);
-		} else if (o instanceof Future) {
-			return convert((Future) o);
-		} else if (o instanceof Guard) {
-			return (Guard) o;
-		} else {
-			System.out.println("Cannot make guard");
-			throw new IllegalArgumentException("Cannot make a guard.");
-		}
-	}
-
-	public static Guard and(Object... guards) {
-		if (guards.length == 0)
-			return null;
-		Guard g = Guard.convert(guards[0]);
-		for (int i = 1; i < guards.length; i++) {
-			g = new ConjunctionGuard(g, Guard.convert(guards[i]));
-		}
-		return g;
-	}
-
-	public static Guard or(Object... guards) {
-		if (guards.length == 0)
-			return null;
-		Guard g = Guard.convert(guards[0]);
-		for (int i = 1; i < guards.length; i++) {
-			g = new DisjunctionGuard(g, Guard.convert(guards[i]));
-		}
-		return g;
-	}
+// probably we should define and(Guard...) and or(Guard...) instead of objects
+//	static private Guard convert(Object o) {
+//		if (o instanceof Supplier) {
+//			//noinspection unchecked
+//			return convert((Supplier<Boolean>) o);
+//		} else if (o instanceof Future) {
+//			return convert((Future) o); // must pass an actor ref!
+//		} else if (o instanceof Guard) {
+//			return (Guard) o;
+//		} else {
+//			System.out.println("Cannot make guard");
+//			throw new IllegalArgumentException("Cannot make a guard.");
+//		}
+//	}
+//
+//	public static Guard and(Object... guards) {
+//		if (guards.length == 0)
+//			return null;
+//		Guard g = Guard.convert(guards[0]);
+//		for (int i = 1; i < guards.length; i++) {
+//			g = new ConjunctionGuard(g, Guard.convert(guards[i]));
+//		}
+//		return g;
+//	}
+//
+//	public static Guard or(Object... guards) {
+//		if (guards.length == 0)
+//			return null;
+//		Guard g = Guard.convert(guards[0]);
+//		for (int i = 1; i < guards.length; i++) {
+//			g = new DisjunctionGuard(g, Guard.convert(guards[i]));
+//		}
+//		return g;
+//	}
 
 
 }
